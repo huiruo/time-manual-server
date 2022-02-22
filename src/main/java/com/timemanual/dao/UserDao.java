422b7566a7dd8a4ca2e4dab0e5c9f0e0ae0c5e87
huiruo <2196411859@qq.com>
2022-02-22 16:10:43 +0800

    feat: remove test user module

--- src/main/java/com/timemanual/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.timemanual.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.timemanual.entity.User;
-
-public interface UserDao extends BaseMapper<User> {
-}
-
--- src/main/java/com/timemanual/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.timemanual.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.timemanual.entity.User;
-
-public interface UserService extends IService<User> {
-}
